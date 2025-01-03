87dcf466c6dc064d98aa4e8da208c7faee9eac95
Rotem Heinig <rotemheinig@icloud.com>
2025-01-03 15:36:50 +0200

    Moved to folders, installed mongoose & body-parser

---
+++ app.js
@@ -0,0 +1,24 @@
+const dotenv = require('dotenv').config();
+const express = require('express');
+const posts = require('./controllers/post');
+
+const indexRouter = require('./routes/');
+const PORT = process.env.PORT;
+
+const app = express();
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL);
+
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to Database'));
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true, limit: '1mb' }));
+app.use(bodyParser.json());
+
+app.use('/', indexRouter);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const app = express();
-const port = process.env.port;
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
